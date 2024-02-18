51380052800d162bf09bc80af40d53e07e75333b
Sergey Timoshin <sergeytimoshin@proton.me>
2024-02-18 18:45:07 +0000

    update bench.sh

--- bench.sh
+++ bench.sh
@@ -5,12 +5,9 @@ for ((batch_size = 1; batch_size <= 10; batch_size++)); do
     echo "Batch size: $batch_size"
     ./gnark-mbu setup --batch-size $batch_size --mode insertion --tree-depth 22 --output circuit_${batch_size}_22
     ./gnark-mbu start --keys-file circuit_${batch_size}_22 --mode insertion --json-logging >> log.txt &
-    # MBU_PID=$!  # Store the PID of gnark-mbu process
-    sleep 3
+    sleep $(batch_size)
     ./gnark-mbu gen-test-params --mode insertion --batch-size $batch_size --tree-depth 22 > ${batch_size}_22_test.json
     curl -X POST -d @${batch_size}_22_test.json http://localhost:3001/prove
-    # Kill gnark-mbu process
-    # kill -9 $MBU_PID
     killall gnark-mbu
     sleep 2
 done
